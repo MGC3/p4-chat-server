1c62c64e4ac6e43dd9f16025e43cf0c527324c3e
michaelchun <michael.chun03@gmail.com>
2020-01-28 14:53:42 -0500

    Add message model

---
+++ app/models/message.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('Message', messageSchema);
