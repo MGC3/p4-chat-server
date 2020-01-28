4c24589a3f373becee5c9cc7f412e40828c2ad0e
michaelchun <michael.chun03@gmail.com>
2020-01-28 15:04:00 -0500

    Import and use message routes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const http = require('http');
 // require route files
 const exampleRoutes = require('./app/routes/example_routes');
 const userRoutes = require('./app/routes/user_routes');
+const messageRoutes = require('./app/routes/message_routes');
 
 // require middleware
 const errorHandler = require('./lib/error_handler');
@@ -73,6 +74,7 @@ app.use(requestLogger);
 // register route files
 app.use(exampleRoutes);
 app.use(userRoutes);
+app.use(messageRoutes);
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
